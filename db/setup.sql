cd0859829141e22b0bd54a21eb4b263a31c95f25
David Parr <parr758@googlemail.com>
2026-06-17 11:55:39 +0100

    Add db/setup.sql with database creation logic

---
+++ db/setup.sql
@@ -0,0 +1,3 @@
+-- Recreate the database from scratch if it already exists to allow a developer to get a clean setup with a single command
+DROP DATABASE IF EXISTS nc_plus_one;
+CREATE DATABASE nc_plus_one;
